ee925575998976501daf4c6aa0d339d996e727e6
a15641404 <499321348@qq.com>
2019-05-21 11:56:17 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# decodewithvideo
+# Decode With Video
+
+
+You need to install these before you running.
+```text
+sudo apt-get install libzbar0
+pip install pyzbar
+pip install opencv-python
+```
---
+++ run.py
@@ -0,0 +1,19 @@
+import cv2
+from pyzbar import pyzbar
+import requests
+found = set()
+capture = cv2.VideoCapture(0)
+while(1):
+    ret,frame = capture.read()
+    test = pyzbar.decode(frame)
+    for tests in test:
+        testdate = tests.data.decode('utf-8')
+
+        if testdate not in found:
+            print('[INFO]Get code:' + testdate)
+            url = 'http://120.78.174.107:8090/add?data=' + testdate
+            requests.post(url)
+            found.add(testdate)
+    #cv2.imshow('Test',frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
